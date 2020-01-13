cc3b9ef88031ce07ab907e6b8d85c9a1eaaaedaa
lwolf83 <58726454+lwolf83@users.noreply.github.com>
2020-01-13 17:33:20 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace codeName
+{
+    class Program
+    {
+        public static string getName()
+        {
+            string name;
+            bool isOnlyLetter;
+            do
+            {
+                Console.WriteLine("Enter your name : ");
+                name = Console.ReadLine().Trim().ToLower();
+                isOnlyLetter = true;
+                foreach (char c in name)
+                {
+                    if (c < 97 || c > 122)
+                    {
+                        isOnlyLetter = false;
+                        Console.WriteLine("Letter '" + c + "' forbidden");
+                    }
+
+                }
+            } while(!isOnlyLetter) ;
+           
+            return name;
+        }
+
+        public static int codeName(string name)
+        {
+            int sumOfLetter = 0;
+            foreach(char c in name)
+            {
+                sumOfLetter = sumOfLetter + (((int)c - 97) % 9)+1;
+            }
+            return sumOfLetter;
+        }
+
+        public static void displayCodedName(String name, int number)
+        {
+            Console.WriteLine(name + " will be coded as " + Convert.ToString(number));
+        }
+
+        static void Main(string[] args)
+        {
+            string stringName;
+            int numberName;
+            stringName = getName();
+            numberName = codeName(stringName);
+            displayCodedName(stringName, numberName);
+
+        }
+    }
+}
